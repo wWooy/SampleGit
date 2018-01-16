d49c30b7182f460aa38df20cb2a88101ee598045
woona <woona@hauri.co.kr>
2018-01-16 14:36:07 +0900

    notification 추가

--- app/src/main/java/com/ww/samplegit/Constants.java
+++ app/src/main/java/com/ww/samplegit/Constants.java
@@ -1,9 +1,33 @@
 package com.ww.samplegit;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
 /**
  * Created by ww on 2018-01-16.
  */
 
 public class Constants {
     public static String VRMATAG = "ViRobot Mobile";
+
+    public void createNotification(Context context, int id){
+
+        Intent intent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pendingIntent);
+        Notification noti = builder.build();
+
+        nm.notify(id, noti);
+
+    }
 }
