54598ca5a3cb3282d5b358408b2d671192216550
woona <woona@hauri.co.kr>
2018-01-16 14:40:54 +0900

    실행할때 현재 시간 로그 추가

--- app/src/main/java/com/ww/samplegit/MainActivity.java
+++ app/src/main/java/com/ww/samplegit/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
             Toast.makeText(this, "lollipop.. ", Toast.LENGTH_SHORT).show();
         }
+
+        Log.d("LOG_TAG", System.currentTimeMillis() + "" );
     }
 }
