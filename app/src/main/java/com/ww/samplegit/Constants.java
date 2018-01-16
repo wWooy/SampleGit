a7c10648f6051d17fb22a441bf7cff40478c6f14
woona <woona@hauri.co.kr>
2018-01-16 14:24:52 +0900

    build 확인 log tag 추가

---
+++ app/src/main/java/com/ww/samplegit/Constants.java
@@ -0,0 +1,9 @@
+package com.ww.samplegit;
+
+/**
+ * Created by ww on 2018-01-16.
+ */
+
+public class Constants {
+    public static String VRMATAG = "ViRobot Mobile";
+}
--- app/src/main/java/com/ww/samplegit/MainActivity.java
+++ app/src/main/java/com/ww/samplegit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ww.samplegit;
 
+import android.app.AlertDialog;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            Toast.makeText(this, "lollipop.. ", Toast.LENGTH_SHORT).show();
+        }
     }
 }
