dbc3e5593b6cd92e50b94baf86ee1a949216b66f
woona <woona@hauri.co.kr>
2018-01-17 13:48:41 +0900

    squashed commit 내용 추가

--- app/src/main/java/com/ww/samplegit/Common.java
+++ app/src/main/java/com/ww/samplegit/Common.java
@@ -6,6 +6,16 @@ package com.ww.samplegit;
 
 public class Common {
     /*raight Merge: 하나의 브랜치와 다른 브랜치의 변경 이력 전체를 합치는 방법이다.
-Squashed Commit: 한 브랜치의 이력을 압축하여 다른 브랜치의 최신 커밋 하나로 만드는 방법이다.
+    Squashed Commit : 커밋합치기
+
+Git이 브랜치 하나의 모든 이력을 압축하여 다른 브랜치에 하나의 커밋으로 만들기에 커밋 합치기라고 한다. 커밋 합치기를 할 때는 유의해야 한다.
+
+
+
+커밋 합치기는 이것저것 실험해 봐야 하는 새로운 기능을 만들거나 버그를 수정할 때 유용하다. 실험한 내용은 추적하지 않아도 되므로 커밋할 필요가 없다. 즉 마지막 결과만 필요할 뿐이다.
+
+
+
+출처: http://goodtogreate.tistory.com/entry/Git의-모든것5-브랜치-사용 [GOOD to GREAT]
 */
 }
