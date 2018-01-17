0158667d9f51ce3fa62d2520f4407aeaee68fb78
woona <woona@hauri.co.kr>
2018-01-17 13:48:13 +0900

    cherry picking 내용 제거

--- app/src/main/java/com/ww/samplegit/Common.java
+++ app/src/main/java/com/ww/samplegit/Common.java
@@ -7,6 +7,5 @@ package com.ww.samplegit;
 public class Common {
     /*raight Merge: 하나의 브랜치와 다른 브랜치의 변경 이력 전체를 합치는 방법이다.
 Squashed Commit: 한 브랜치의 이력을 압축하여 다른 브랜치의 최신 커밋 하나로 만드는 방법이다.
-Cherry-picking: 다른 브랜치에서 하나의 커밋을 가져와서 현재 브랜치에 적용하는 방법이다.
 */
 }
